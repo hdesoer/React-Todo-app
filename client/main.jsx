9418f3715a9702e5632478116217414bd0cf823d
hdesoer <hdesoer@126.com>
2016-07-04 01:47:36 +0800

    collections

--- client/main.jsx
+++ client/main.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
  
-import App from '../client/ui/App.jsx';
+import App from '../imports/ui/App.jsx';
  
 Meteor.startup(() => {
   render(<App />, document.getElementById('render-target'));
-});
+}); 
